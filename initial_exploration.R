d85d02a268c681a5d4b73f6994929711b92157e5
bwang68 <bubwb12@gmail.com>
2022-07-12 15:30:34 -0400

    data used and initial graph

---
+++ initial_exploration.R
@@ -0,0 +1,14 @@
+#### Load in csv file ####
+
+phone_benchmarks <- read.csv("data/ML_ALL_benchmarks.csv")
+
+## practice plotting with data
+
+library(ggplot2)
+
+ggplot(data = phone_benchmarks, aes(cpuScore)) +
+  geom_histogram(binwidth = 30, aes(fill = company)) +
+  labs(x = "CPU Performance Score (higher is better)", y = "Count") +
+  scale_fill_brewer(palette = "Reds") + 
+  theme_dark()
+

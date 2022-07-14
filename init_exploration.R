24f74ff5466d9aaf51abecd02cd26182f67b932f
bwang68 <bubwb12@gmail.com>
2022-07-14 13:44:21 -0400

    new exploration graph

--- init_exploration.R
+++ init_exploration.R
@@ -10,3 +10,23 @@ ggplot(data = phone_benchmarks, aes(cpuScore)) +
   theme_dark()
 
 
+## practice subsetting data
+# use a combo of filter, select, mutate, arrange, summarise, group_by, sample_n and/or slice, rename
+# create visulization using new datasets
+
+phone_benchmarks %>% 
+  filter(company == "Qualcomm") %>% 
+  ggplot(aes(cpuScore)) + 
+  geom_histogram(binwidth = 30)
+
+phone_benchmarks %>% 
+  filter(company == "Apple") %>% 
+  ggplot(aes(cpuScore)) + 
+  geom_histogram(binwidth = 70)
+
+
+
+# sidenote for changing a certain value in a column to something else we can do
+# data$colname <- sub(val_to_change, new_value, data$colname)
+
+

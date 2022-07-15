05a8d0d00d4b84d388dc597cc770461679511be2
bwang68 <bubwb12@gmail.com>
2022-07-15 15:29:44 -0400

    more exploration with dplyr and barplot

--- init_exploration.R
+++ init_exploration.R
@@ -29,4 +29,48 @@ phone_benchmarks %>%
 # sidenote for changing a certain value in a column to something else we can do
 # data$colname <- sub(val_to_change, new_value, data$colname)
 
+phone_benchmarks %>% 
+  filter(company == "Qualcomm" | company == "Apple") %>% 
+  ggplot(aes(cpuScore, color = company)) + 
+  geom_boxplot()
+
+phone_benchmarks %>% 
+  filter(grepl('iPhone|Samsung', device)) %>%
+  arrange(desc(cpuScore)) %>%
+  ggplot(aes(cpuScore, fill = company)) + 
+  geom_histogram(binwidth = 50) +
+  labs(x = "Cpu Score, higher is better",
+       y = "Count of phones",
+       title = "Phone CPU benchmarks, Apple vs Qualcomm vs Samsung") +
+  scale_fill_brewer(palette = "Set2")
+  
+
+# practice w dplyr
+
+library(stringr)
+
+android_only <- phone_benchmarks %>% 
+  filter(company != "Apple") %>% 
+  select(device, company, cpuName, clock, cpuScore, gpuScore) %>% 
+  mutate(totalScore = cpuScore + gpuScore) %>% 
+  mutate(cpuMaker = company) %>% 
+  # want to select the first word from device for phone maker name
+  # use stringr library
+  mutate(phone_company = word(device, 1)) %>% 
+  #notice that ASUS and Asus are separate, need to join these grps together
+  mutate(phone_company = str_replace(phone_company, "ASUS", "Asus"))
+
+
+android_only  %>% 
+  ggplot(aes(phone_company, totalScore)) +
+  geom_boxplot()
+
+
+#look at summary stats
+
+android_only %>% 
+  group_by(phone_company) %>% 
+  summarize(mean_total_score = mean(totalScore, na.rm = TRUE))
+
+
 
